063317ee056f72a9d048f6c50bf2a0677338934e
KumaTea <KumaTea@outlook.com>
2023-11-19 20:51:54 +0800

    aarch64 --> riscv64

--- build/build.sh
+++
@@ -1,9 +0,0 @@
-set -xe
-
-echo "Building PyTorch"
-
-
-export CC=/opt/rh/devtoolset-8/root/usr/bin/gcc
-export GCC=/opt/rh/devtoolset-8/root/usr/bin/gcc
-export CPP=/opt/rh/devtoolset-8/root/usr/bin/cpp  # not g++
-export CXX=/opt/rh/devtoolset-8/root/usr/bin/g++
--- src/main.py
+++
@@ -1,6 +0,0 @@
-from index import gen_index, gen_index_cn
-
-
-if __name__ == '__main__':
-    gen_index()
-    gen_index_cn()
